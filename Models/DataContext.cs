320301488a92bc01e600ae88717ee9a91dbe3cec
Pavel Kutakov <p00565754@china.huawei.com>
2020-05-06 12:11:37 +0300

    initial

---
+++ Models/DataContext.cs
@@ -0,0 +1,20 @@
+using Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Models
+{
+  public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }      
+    }   
+}
---
+++ Models/Sale.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models{
+    public class Sale {
+        public int ID {get; set;}
+
+        public int ProductID {get; set;}
+        public Product Product {get; set;}
+        public int ClientID {get; set;}
+        public Client Client {get; set;}
+        public int Quantity{get; set;}
+        public decimal Price {get; set;}
+        public decimal TotalAmount {get; set; }
+        public decimal LastClientBalance {get; set;}
+    }
+}
