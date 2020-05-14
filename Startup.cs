613205ab94ca88dfb57bc9b433678f01e35e9247
Pavel Kutakov <p00565754@china.huawei.com>
2020-05-14 17:56:31 +0300

    MIgration

--- Startup.cs
+++ Startup.cs
@@ -38,7 +38,7 @@ namespace application
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataContext context)
         {
             if (env.IsDevelopment())
             {
@@ -55,6 +55,7 @@ namespace application
             {
                 endpoints.MapControllers();
             });
+            context.Database.Migrate();
         }
     }
 }
